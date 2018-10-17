48c6feaf90bee9ad565bdc88fed1eec56dbccf24
Daniel Lemire <lemire@gmail.com>
2018-10-17 18:03:05 -0400

    clang-format

--- include/simdasciicheck.h
+++ include/simdasciicheck.h
@@ -1,16 +1,16 @@
 #ifndef SIMDASCIICHECK_H
 #define SIMDASCIICHECK_H
 
-#include <stddef.h> // size_t
-#include <stdbool.h> // c99 bool
 #include <emmintrin.h> // SSE2
+#include <stdbool.h>   // c99 bool
+#include <stddef.h>    // size_t
 
-// The function returns true (1) if all chars passed in src are 
+// The function returns true (1) if all chars passed in src are
 // 7-bit values (0x00..0x7F). Otherwise, it returns false (0).
 static bool validate_ascii_fast(const char *src, size_t len) {
   size_t i = 0;
   __m128i has_error = _mm_setzero_si128();
-  if(len >= 16) {
+  if (len >= 16) {
     for (; i <= len - 16; i += 16) {
       __m128i current_bytes = _mm_loadu_si128((const __m128i *)(src + i));
       has_error = _mm_or_si128(has_error, current_bytes);
@@ -29,12 +29,12 @@ static bool validate_ascii_fast(const char *src, size_t len) {
 
 #ifdef __AVX2__
 #include <x86intrin.h>
-// The function returns true (1) if all chars passed in src are 
+// The function returns true (1) if all chars passed in src are
 // 7-bit values (0x00..0x7F). Otherwise, it returns false (0).
 static bool validate_ascii_fast_avx(const char *src, size_t len) {
   size_t i = 0;
   __m256i has_error = _mm256_setzero_si256();
-  if(len >= 32) {
+  if (len >= 32) {
     for (; i <= len - 32; i += 32) {
       __m256i current_bytes = _mm256_loadu_si256((const __m256i *)(src + i));
       has_error = _mm256_or_si256(has_error, current_bytes);
@@ -51,8 +51,5 @@ static bool validate_ascii_fast_avx(const char *src, size_t len) {
   return !error_mask;
 }
 #endif
-
-
-
 
 #endif
